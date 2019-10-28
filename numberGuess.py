abb9551bdb3301ecf4ca3b8d173947b9294d99aa
C3LY <43711652+C3LY@users.noreply.github.com>
2019-10-28 01:01:36 +0000

    Update numberGuess.py

--- numberGuess.py
+++ numberGuess.py
@@ -1,1 +1,29 @@
+import random
+print("Hello")
+userInputNumber=0
+playGame=True
+while(playGame==True):
+  win=False
+  x=random.randint(1,10)
+  userInputNumber=0
+  print("pick a number")
+  while(win==False):
+    userInput = input("type here:")
+    userInputNumber = int(userInput)
+    print(userInputNumber)
+    if(userInputNumber<x):
+     print("too low")
+    elif(userInputNumber>x):
+      print("too high")
+    else:
+     print("You win!")
+     win=True
+  playAgainUserInput = input("Would you like to play again? y for yes, n for no")
+  if(playAgainUserInput!="y"):
+    playGame=False
+print("closing...")
 
+#other challenge:
+# 2 player mode
+#menu
+#robustness
